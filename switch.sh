01ffd6cc737b60f376b8a9a7c1956302b258b1fa
Scott Murphy <scott@alwaysvip.com>
2021-02-21 11:49:48 -0800

    Script for switching between various versions

---
+++ switch.sh
@@ -0,0 +1,17 @@
+if [ $# -lt 1 ]; then
+  echo "switch.sh release/master/ryaa"
+  exit
+fi
+cordova plugin remove cordova-plugin-camera
+if [ "$1" == "release" ]; then
+  VERSION=cordova-plugin-camera
+elif [ "$1" == "master" ]; then
+  VERSION=https://github.com/apache/cordova-plugin-camera.git
+elif [ "$1" == "ryaa" ]; then
+  VERSION=https://github.com/ryaa/cordova-plugin-camera.git
+else
+  echo "Unknown version '$1'. Installing release instead."
+  VERSION=cordova-plugin-camera
+fi
+echo "\n*** Installing \"$VERSION\"...\n"
+cordova plugin add $VERSION
