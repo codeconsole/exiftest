09b3a550abe37e34506eb8b7d14da1eb34699c95
Scott Murphy <scott@alwaysvip.com>
2021-02-22 10:47:38 -0800

    Add pull request to switch script.

--- switch.sh
+++ switch.sh
@@ -9,6 +9,8 @@ elif [ "$1" == "master" ]; then
   VERSION=https://github.com/apache/cordova-plugin-camera.git
 elif [ "$1" == "ryaa" ]; then
   VERSION=https://github.com/ryaa/cordova-plugin-camera.git
+elif [ "$1" == "pullrequest" ]; then
+  VERSION=https://github.com/codeconsole/cordova-plugin-camera.git#ryaa
 else
   echo "Unknown version '$1'. Installing release instead."
   VERSION=cordova-plugin-camera
