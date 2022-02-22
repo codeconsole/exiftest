49f2f4ba29e0b6c1cd68bda3b085740c311a256f
Scott Murphy <scott@alwaysvip.com>
2022-02-22 17:46:53 -0500

    Pick Images support

--- www/js/index.js
+++ www/js/index.js
@@ -10,11 +10,18 @@ function onDeviceReady() {
         document.getElementById('PHOTOLIBRARY').onclick = function() {
             processPhoto('PHOTOS')
         }
+        document.getElementById('PICKIMAGES').onclick = function() {
+            processPhoto('PICKIMAGES')
+        }
     }
 }
 
 
 function processPhoto(source) {
+    var pickImages = source == 'PICKIMAGES'
+    if (pickImages) {
+        source = 'PHOTOS'
+    }
     // https://github.com/ionic-team/capacitor/blob/2.4.9/core/src/core-plugin-definitions.ts
     // https://github.com/ionic-team/capacitor/blob/main/core/src/definitions.ts
     // https://github.com/ionic-team/capacitor-plugins/blob/main/camera/src/definitions.ts
@@ -28,33 +35,47 @@ function processPhoto(source) {
                 }
                 return
             })
-        } else if (permissions[source.toLowerCase()] == 'denied') {
-            alert('You have not granted access to use the '+source.toLowerCase())
-            return 
-        }
+        } else if (permissions[source.toLowerCase()] != 'granted') {
+            alert('You do not have permission to access \'+source.toLowerCase()+\'')
+            return
+        }            
         Capacitor.Plugins.Geolocation.requestPermissions().then(function(permissionStatus) {    
             alert(JSON.stringify(permissionStatus)) 
-            Capacitor.Plugins.Camera.getPhoto({
-              quality: parseInt($('input[name=quality]').val()),
-              resultType: $('select[name=destinationType]').val() == 'FILE_URI'? 'uri' : 'dataUrl',
-              allowEditing: false,
-              correctOrientation: $('input[name=correctOrientation]').val() == 'true',
-              source: source
-            }).then(function(photo) {
-                $('#selectedImage').attr('src', photo.dataUrl? photo.dataUrl : photo.webPath).css('visibility', 'visible')
-
-                if (photo.dataUrl) {
-                    var blob = CameraUtil.stringToBlob(photo.dataUrl.substring('data:image/jpeg;base64,'.length))
-                    photo.dataUrl = '[HIDDEN]'
-                    CameraUtil.showResult(blob, photo)
-                } else{
-                    Capacitor.Plugins.Filesystem.readFile({path: photo.path}).then(function(file) {
-                        CameraUtil.showResult(CameraUtil.stringToBlob(file.data), photo)
-                    })
-                }
-            }).catch(function(error) {
-                alert(JSON.stringify(error))
-            })     
+            var opts = { 
+                quality: parseInt($('input[name=quality]').val()),
+                resultType: $('select[name=destinationType]').val() == 'FILE_URI'? 'uri' : 'dataUrl',
+                allowEditing: false,
+                correctOrientation: $('input[name=correctOrientation]').val() == 'true'
+            }
+            if (pickImages) {
+                opts.limit = 1
+                Capacitor.Plugins.Camera.pickImages(opts).then(function(result) { 
+                    PhotoUtil.displayPhoto(result.photos[0])
+                }).catch(PhotoUtil.displayError) 
+            } else {          
+                opts.source = source
+                Capacitor.Plugins.Camera.getPhoto(opts).then(PhotoUtil.displayPhoto).catch(PhotoUtil.displayError)   
+            }  
         })   
     })
 }
+
+var PhotoUtil = {
+    displayPhoto: function(photo) {
+        $('#selectedImage').attr('src', photo.dataUrl? photo.dataUrl : photo.webPath).css('visibility', 'visible')
+
+        if (photo.dataUrl) {
+            var blob = CameraUtil.stringToBlob(photo.dataUrl.substring('data:image/jpeg;base64,'.length))
+            photo.dataUrl = '[HIDDEN]'
+            CameraUtil.showResult(blob, photo)
+        } else{
+            Capacitor.Plugins.Filesystem.readFile({path: photo.path}).then(function(file) {
+                CameraUtil.showResult(CameraUtil.stringToBlob(file.data), photo)
+            })
+        }
+    },
+    displayError: function(error) {
+        alert(JSON.stringify(error))
+    }   
+}
+
